1d23619a7f045df757a9f97db8438a9f62571492
ramu <ramu25251944@gmail.com>
2023-06-06 12:44:53 +0530

    hello world

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public void data()
+	{
+		System.out.println("hello world");
+	}
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.data();
+
+	}
+
+}
